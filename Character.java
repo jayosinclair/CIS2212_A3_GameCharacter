b1f33f27e78dec683414fe650fe29b486d5d149b
josinclair <jay.olson@sinclair.edu>
2026-02-09 18:59:28 -0500

    Set up shell for Character class and a file to test it, CharacterTest.java.

---
+++ Character.java
@@ -0,0 +1,25 @@
+/**
+ * The Character class contains a blueprint for character attributes and actions. It contains accessor/mutator (getter/setter) methods.
+ * 
+ */
+
+public class Character {
+    
+    //FIXME: Where do I instantiate the random generator? Here? Elsewhere?
+
+    //******************************************************MEMBER VARIABLES********************************************************
+
+    private int id;
+    private String name;
+    private int level;
+    private int health;
+    private int attack;
+    private int magic;
+    private int defense;
+    private int luck;
+    
+     //*******************************************************MEMBER METHODS********************************************************
+
+    
+
+}
---
+++ CharacterTest.java
@@ -0,0 +1,29 @@
+//Author: Jay Olson
+//Section: CIS 2212-801
+//Assignment: Project 3 Game Character
+//Due Date: February 25, 2026
+//Submitted: February XX, 2026
+
+/**
+Program Purpose: Practice designing a class.
+
+Program Function: This program designs a class for an RPG game character. The class has accessor and mutator methods along 
+with a couple other methods used to manipulate character attributes and have characters take actions.
+
+Note: CharacterTest is the wrapper class containing the main method, while the Character class has the blueprint for the 
+game character's member variables and methods.
+
+*/
+
+//Github repo for this project is at: https://github.com/jayosinclair/CIS2212_A3_GameCharacter.git
+//**********************************************************************************************************************
+
+import java.util.Scanner;
+
+public class CharacterTest{
+
+    public static void main(String[] args) {
+
+
+
+    }
